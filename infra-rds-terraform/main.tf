ee1a066fe3ddde7594339f3646a421e7b80dd3ed
lfneves <lfneves.dg@gmail.com>
2023-10-20 04:01:49 -0300

    fix: update rds repository

--- infra-rds-terraform/main.tf
+++ infra-rds-terraform/main.tf
@@ -58,7 +58,7 @@ resource "aws_db_instance" "postgresql" {
   multi_az                        = var.multi_availability_zone
   port                            = var.database_port
   vpc_security_group_ids          = var.security_group
-  db_subnet_group_name            = aws_db_subnet_group.subnet_group
+  db_subnet_group_name            = aws_db_subnet_group.subnet_group.name
   parameter_group_name            = var.parameter_group
   storage_encrypted               = var.storage_encrypted
   monitoring_interval             = var.monitoring_interval
